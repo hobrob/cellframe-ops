0eaf6d0c6826e1f011e4b9b4b2a080e126053a50
hobrob <30702647+hobrob@users.noreply.github.com>
2024-10-20 23:31:28 +0100

    Update build_and_run.sh

--- cellframe-docker/build_and_run.sh
+++ cellframe-docker/build_and_run.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-if [[ $# -lt 1 ]] ; then
-    echo "Usage: ./build_and_run.sh <name> [-d] (-d for daemonizing)"
-    exit 1
-    
-elif [[ $# -eq 2 ]]; then
-    if [[ "$2" != "-d" ]] ; then
+if [ $# -lt 1 ]; then
+    echo "Usage: ./build_and_run.sh <name> [-d] (for daemonizing) [<major version number> <minor version number>] (for specifying a version, specify both major and minor or no effect)"
+    exit 1    
+elif [ $# -eq 2 ] || [ $# -eq 4 ]; then
+    if [ "$2" != "-d" ]; then
             echo "Unsupported argument: $2, exiting..."
             exit 1
     else
-        docker build -t "$1" .
-        docker volume create "$1"
-        docker run -v "$1":/opt/cellframe-node --name="$1" --privileged --net=host -it -d "$1" 
+            DAEMON="d"
+                if [ $# -eq 4 ]; then
+                        VMAJOR=$3
+                        VMINOR=$4
+                else
+                        source latest_version
+                fi
     fi
-
+elif [ $# -eq 3 ]; then
+        VMAJOR=$2
+        VMINOR=$3
 else
-    docker build -t "$1" .
-    docker volume create "$1"
-    docker run -v "$1":/opt/cellframe-node --name="$1" --privileged --net=host -it "$1"
+        source latest_version
 fi
+docker build -t "$1" --build-arg VMAJOR="$VMAJOR" --build-arg VMINOR="$VMINOR" .
+docker volume create "$1"
+docker run -v "$1":/opt/cellframe-node --name="$1" --privileged --net=host -it"$DAEMON" "$1"
